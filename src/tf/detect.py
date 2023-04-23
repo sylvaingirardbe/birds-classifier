1eace1ab8799a9e1c89c104f92eb077cc55c9fe9
Sylvain Girard <sylvainx_girardx@hotmail.com>
2023-04-23 23:54:45 +0200

    BVleh

--- src/tf/detect.py
+++ src/tf/detect.py
@@ -53,14 +53,14 @@ def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
   fps_avg_frame_count = 10
 
   # Initialize the object detection model
-#   base_options = core.BaseOptions(
-#       file_name=model, use_coral=enable_edgetpu, num_threads=num_threads)
-#   detection_options = processor.DetectionOptions(
-#       max_results=3, score_threshold=0.3)
-#   options = vision.ObjectDetectorOptions(
-#       base_options=base_options, detection_options=detection_options)
-#   detector = vision.ObjectDetector.create_from_options(options)
-  detector = vision.ObjectDetector.create_from_file(model)
+  base_options = core.BaseOptions(
+      file_name=model, use_coral=enable_edgetpu, num_threads=num_threads)
+  detection_options = processor.DetectionOptions(
+      max_results=3, score_threshold=0.3)
+  options = vision.ObjectDetectorOptions(
+      base_options=base_options, detection_options=detection_options)
+  detector = vision.ObjectDetector.create_from_options(options)
+#   detector = vision.ObjectDetector.create_from_file(model)
 
   # Continuously capture images from the camera and run inference
   while cap.isOpened():
