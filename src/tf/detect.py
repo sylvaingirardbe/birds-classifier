326763b2624400fdff147d59ffb92aabbd9e4bb9
Sylvain Girard <sylvainx_girardx@hotmail.com>
2023-04-24 19:28:06 +0200

    Output detection_result

--- src/tf/detect.py
+++ src/tf/detect.py
@@ -45,11 +45,11 @@ def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
   cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
   # Visualization parameters
-  row_size = 20  # pixels
-  left_margin = 24  # pixels
-  text_color = (0, 0, 255)  # red
-  font_size = 1
-  font_thickness = 1
+#   row_size = 20  # pixels
+#   left_margin = 24  # pixels
+#   text_color = (0, 0, 255)  # red
+#   font_size = 1
+#   font_thickness = 1
   fps_avg_frame_count = 10
 
   # Initialize the object detection model
@@ -82,25 +82,27 @@ def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
     # Run object detection estimation using the model.
     detection_result = detector.detect(input_tensor)
 
+    print(detection_result)
+
     # Draw keypoints and edges on input image
-    image = utils.visualize(image, detection_result)
+    # image = utils.visualize(image, detection_result)
 
     # Calculate the FPS
-    if counter % fps_avg_frame_count == 0:
-      end_time = time.time()
-      fps = fps_avg_frame_count / (end_time - start_time)
-      start_time = time.time()
+    # if counter % fps_avg_frame_count == 0:
+    #   end_time = time.time()
+    #   fps = fps_avg_frame_count / (end_time - start_time)
+    #   start_time = time.time()
 
     # Show the FPS
-    fps_text = 'FPS = {:.1f}'.format(fps)
-    text_location = (left_margin, row_size)
-    cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
-                font_size, text_color, font_thickness)
+    # fps_text = 'FPS = {:.1f}'.format(fps)
+    # text_location = (left_margin, row_size)
+    # cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+    #             font_size, text_color, font_thickness)
 
     # Stop the program if the ESC key is pressed.
     if cv2.waitKey(1) == 27:
       break
-    cv2.imshow('object_detector', image)
+    # cv2.imshow('object_detector', image)
 
   cap.release()
   cv2.destroyAllWindows()
